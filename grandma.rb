3ff5204f45f19eabd53393d41e7495386f5f61c6
Phillip DiPhillipo <diphillipophillip@gmail.com>
2018-10-14 22:48:17 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,14 @@
 def speak_to_grandma(phrase)
 
 if phrase == phrase.upcase
-  puts "I LOVE YOU TOO PUMPKIN"
+  "NO, NOT SINCE 1938!"
+else if phrase == "I LOVE YOU GRANDMA!"
+  "I LOVE YOU TOO PUMPKIN!"
 else
   "HUH?! SPEAK UP, SONNY!"
+end 
+end
+  
   
   
 # Whatever you say to grandma, she should respond with
